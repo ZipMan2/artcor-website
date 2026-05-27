6cefd95486e16893adcc1a19b9099f39b2021185
ZipMan <janwisniewski2@proton.me>
2026-05-27 23:34:25 +0200

    feat: add Quote section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,52 @@
 import Features from '@/components/sections/Features'
 import Hero from '@/components/sections/Hero'
 import Products from '@/components/sections/Products'
+import Quote from '@/components/sections/Quote'
 
 export default function Home() {
   return (
     <main>
       {/* Hero Section */}
       <Hero />
-
       {/* Features Section */}
       <Features />
-
       {/* Products Section */}
       <Products />
-
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-p <br />
-
-
-
+      {/* Quote Section */}
+      <Quote />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
+      p <br />
     </main>
   )
 }
---
+++ src/components/sections/Quote.tsx
@@ -0,0 +1,18 @@
+export default function Quote() {
+  return (
+    <section className="w-full bg-white">
+      <div className="mx-auto flex max-w-7xl flex-col items-center justify-center px-6 py-16 text-center md:max-w-2xl md:py-10">
+        <h3 className="mb-2 text-xl leading-relaxed font-medium sm:text-2xl md:text-4xl">
+          “Wsłuchiwanie się w głos
+          <br className="hidden sm:block" /> klientów powinno stać się
+          <br className="hidden sm:block" /> dla biznesem każdej
+          firmy.”
+        </h3>
+
+        <span className="text-sm font-medium text-stone-700 sm:text-base md:ml-auto md:block md:pr-6 md:text-lg">
+          - Tom Peters
+        </span>
+      </div>
+    </section>
+  )
+}
